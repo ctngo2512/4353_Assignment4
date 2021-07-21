425ffecfc8842871d213de50f20a941b2cf778f8
Chi Ngo <70230102+ctngo2512@users.noreply.github.com>
2021-07-21 12:34:22 -0500

    cleaning

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,22 +1,23 @@
-import React from 'react';
-import {shallow, mount} from 'enzyme';
+import React, {useEffect} from 'react';
 import App from './App';
-import { act, render } from '@testing-library/react';
 import Hero from './Hero';
-import {handleLogout} from './Hero';
+import {handleLogout} from './App';
+import {authListener} from './App';
+import {configure, shallow, mount} from 'enzyme';
+import {act, render} from '@testing-library/react';
+import fire from './fire';
 
 describe('App component tests', ()=> {
-    
+      
     it("renders without crashing", () => {
         shallow(<App />);
     });
 
     test('should handle login and hero', () => {
-
         const wrapper = shallow(<App />);
-        
+        expect(wrapper).toMatchSnapshot();
         const wrapper2 = mount(<Hero handleLogout = {handleLogout}/>);
-
-    })
+        expect(wrapper2).toMatchSnapshot();
+    });
 
 });
